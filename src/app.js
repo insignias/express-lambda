f72d116caa3286302c188075cf725e096f6c979d
Sharan <issharan@88665a345be3.ant.amazon.com>
2021-11-13 17:02:55 -0500

    Fail Service Stack temproraily

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send("Hello World !");
+    res.sendStatus(500);
+    // res.send("Hello World !");
 });
 
 const port = 8080;
